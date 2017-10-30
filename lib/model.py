69e7d6c47092fa1244584b200e74e78ba1ab8d26
ahessler01 <Anna@Annas-MacBook-Pro.local>
2017-10-30 13:47:14 -0600

    trying basic compile

--- lib/model.py
+++ lib/model.py
@@ -27,15 +27,15 @@ class ImageBranch(Sequential):
         #want to flatten it together with weather data in model class
 
 
-        # self.add(Dense(32, activation='relu', input_dim=(inputSize)))
+        self.add(Dense(32, activation='relu', input_dim=(input_shape)))
         # self.add(Dropout(0.5))
-        # self.add(Dense(1, activation='sigmoid'))
+        self.add(Dense(1, activation='sigmoid'))
 
         # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
-        # self.compile(optimizer='rmsprop',
-        #       loss='binary_crossentropy',
-        #       metrics=['accuracy'])
+        self.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
 
     def fit(self, traindata, testdata):
 
