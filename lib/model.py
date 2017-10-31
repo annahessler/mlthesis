38a1e76672e66edfaca2c339b706177772793618
Nick Crews <nicholas.b.crews@gmail.com>
2017-10-31 10:20:30 -0600

    Added model visualization and tweaked network

--- lib/model.py
+++ lib/model.py
@@ -20,7 +20,7 @@ class ImageBranch(Sequential):
         img_y = aoisize[1]
         input_shape = (img_x, img_y, nchannels)
 
-        self.add(Conv2D(32, kernel_size=(3,3), strides=(1,1),
+        self.add(Conv2D(64, kernel_size=(3,3), strides=(1,1),
                         activation='relu',
                         input_shape=input_shape))
         self.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
@@ -30,22 +30,22 @@ class ImageBranch(Sequential):
         self.add(Dense(128, activation='relu'))
 
         self.compile(optimizer='rmsprop',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
+            loss='binary_crossentropy',
+            metrics=['accuracy'])
 
 class FireModel(Model):
 
     def __init__(self, weatherDataSize, spatialChannels, aoiSize):
-        print("creating network with shape", weatherDataSize, spatialChannels, aoiSize)
+        # print("creating network with shape", weatherDataSize, spatialChannels, aoiSize)
         self.wb = Input((weatherDataSize,))
         self.ib = ImageBranch(spatialChannels, aoiSize)
 
-        print('weather branch info:', self.wb.shape)
-        print('image branch info:', self.ib.input_shape, self.ib.output_shape, self.ib.output)
+        # print('weather branch info:', self.wb.shape)
+        # print('image branch info:', self.ib.input_shape, self.ib.output_shape, self.ib.output)
 
         concat = Concatenate()([self.wb,self.ib.output])
         out = Dense(1, kernel_initializer = 'normal', activation = 'sigmoid')(concat)
-        print("concat and out info:", concat.shape, out.shape)
+        # print("concat and out info:", concat.shape, out.shape)
         super().__init__([self.wb, self.ib.input], out)
 
         # self.add(Concatenate([self.wb, self.ib]))
@@ -55,7 +55,7 @@ class FireModel(Model):
 
     def fit(self, trainData):
         weather,spatialData, outputs = trainData.getData()
-        super().fit([weather, spatialData], outputs, batch_size = 2000, epochs = 50, verbose = 1)
+        super().fit([weather, spatialData], outputs, batch_size = 1000, epochs = 50, verbose = 1)
 
     def predict(self, dataset):
         weather,spatialData, outputs = dataset.getData()
--- main.py
+++ main.py
@@ -4,21 +4,23 @@ import numpy as np
 import lib.model
 from lib.datamodule import Data
 from lib.dataset import Dataset
+from lib import viz
 
 
 
 data = Data.defaultData('0731')
 dataset = Dataset(data)
-usedLayers = ['ndvi', 'slope', 'aspect']
+usedLayers = ['perim', 'ndvi', 'slope', 'aspect']
 dataset.usedLayers = usedLayers
-dataset.usedWeather = ['maxTemp','avgHum']
+# dataset.usedWeather = ['maxTemp','avgHum']
+dataset.usedWeather = []
 trainData, testData = dataset.split()
 
 weather, aois, out = trainData.getData()
 nsamples, height, width, nchannels = aois.shape
 
 mod = lib.model.FireModel(weather.shape[1], nchannels, (height, width))
-mod.fit(trainData)
-res = mod.predict(testData)
-
-print(res)
+viz.saveModel(mod)
+# mod.fit(trainData)
+# res = mod.predict(testData)
+# print(res)
